e346f22c1f305d923349cd91d574a5baba19fc31
mvmolloy <mvamolloy@gmail.com>
2021-08-02 14:50:39 +0000

    add crt logo

--- src/index.js
+++ src/index.js
@@ -60,6 +60,7 @@ function Form() {
                 </p>
             )}
             <div className="form-wrapper">
+                <img className="logo-main" src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Canal_%26_River_Trust_Logo_v2.png" alt="Canal and River Trust Logo" />
                 <h1>Contact Us</h1>
                 <form onSubmit={sendEmail}>
                     <p>
