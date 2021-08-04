090469f5f286bd61cb2915fa4bf4cf2b67a655d1
mvmolloy <mvamolloy@gmail.com>
2021-08-04 10:09:51 +0000

    add result component

---
+++ src/components/Result.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class FormResult extends React.Component {
+    render() {
+        const result = this.props.result;
+        return(
+            <div className="result-wrapper">
+                <p className={`${result.success ? 'success' : 'error'}`}>
+                    {result.message}.
+                </p>
+            </div>
+        )
+    }
+}
+
+export default FormResult;
